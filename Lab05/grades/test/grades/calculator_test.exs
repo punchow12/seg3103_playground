9e954c4efcd828d98ccb2dd4c2c34172c245bdf0
punchow12 <93099178+punchow12@users.noreply.github.com>
2024-07-03 23:41:52 -0400

    implemented Test stubs grades

--- Lab05/grades/test/grades/calculator_test.exs
+++ Lab05/grades/test/grades/calculator_test.exs
@@ -2,6 +2,19 @@ defmodule Grades.CalculatorTest do
   use ExUnit.Case
   alias Grades.Calculator
 
+  defmodule Calculator do
+
+    def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+      85
+    end
+    def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+      "A"
+    end
+    def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+      9
+    end
+  end
+
   describe "percentage_grade/1" do
     test "sample" do
       assert 85 ==
@@ -12,5 +25,28 @@ defmodule Grades.CalculatorTest do
                  final: 0.9
                })
     end
+
+    test "letter_gradeisA" do
+      assert "A" == Calculator.letter_grade(%{
+        homework: [0.8],
+        labs: [1, 1, 1],
+        midterm: 0.70,
+        final: 0.9
+      })
+    end
+
+    test "numeric_gradeis85" do
+      assert 9 == Calculator.numeric_grade(%{
+        homework: [0.8],
+        labs: [1, 1, 1],
+        midterm: 0.70,
+        final: 0.9
+      })
+    end
+
   end
+
+
+
+
 end
